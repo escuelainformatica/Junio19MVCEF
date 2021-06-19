abe37b52d099d295067f5121389b867c176503c6
Jorge Castro <jorge.castro.c@gmail.com>
2021-06-19 10:28:53 -0400

    ok

---
+++ Junio19MVCEF/controlador/ParteController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Junio19MVCEF.controlador
+{
+    // 1) Tiene que terminar con el nombre Controller (no Controller1)
+    // 2) Tiene que heredar la clase Controller
+    // 3) Tiene que tener metodos que regresan un IActionResult
+
+    public class ParteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        // http://localhost:45374/Parte/Ejemplo
+        public IActionResult Ejemplo()
+        {
+
+            return View();
+        }
+    }
+}
